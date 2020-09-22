b70930efb4dd5bfc524c185a66bce140cc0a46d2
Chetan77verma <prem.premsharma77@gmail.com>
2020-09-22 19:01:00 +0530

    edit employee and  some refactor code

--- src/components/employeeform.svelte
+++ src/components/employeeform.svelte
@@ -1,31 +1,61 @@
 <script>
   import { createEventDispatcher } from "svelte";
-
   const dispatch = createEventDispatcher();
-  let employee = {};
+
+  export let editingEmployee = {};
+  $: employee = editingEmployee;
   const apiBaseUrl = "http://dummy.restapiexample.com/api/v1";
   let loading = false;
+
   const onSubmit = async (e) => {
     e.preventDefault();
-    if (employee.name && employee.age && employee.salary) {
+    if (
+      employee.employee_name &&
+      employee.employee_age &&
+      employee.employee_salary &&
+      !employee.id
+    ) {
+      //create call
+      let empobj = {
+        name: employee.employee_name,
+        age: employee.employee_age,
+        salary: employee.employee_salary,
+      };
       loading = true;
       const res = await fetch(`${apiBaseUrl}/create`, {
         method: "POST",
-        body: employee,
+        body: empobj,
       });
       res
         .json()
         .then((data) => {
           loading = false;
-          dispatch("postCreate", data.data);
           employee = {};
-          console.log(data);
-          if (data.status == "success") {
-            console.log(data.message);
-          }
+          dispatch("postCreate", data.message);
         })
         .catch((err) => {
-          console.log(err);
+          alert(err);
+          loading = false;
+          employee = {};
+        });
+    } else if (employee.id) {
+      //update call
+      loading = true;
+      fetch(`${apiBaseUrl}/update/${employee.id}`, {
+        method: "PUT",
+        body: employee,
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          //success
+          loading = false;
+          employee = {};
+          dispatch("postUpdate", data.message);
+        })
+        .catch((err) => {
+          alert(err);
           loading = false;
           employee = {};
         });
@@ -36,23 +66,32 @@
 </script>
 
 <style>
+  form {
+    margin: 30px 10px;
+  }
 </style>
 
 {#if !loading}
   <form on:submit={onSubmit}>
     <div class="input-field">
-      <label for="name">Name</label>
-      <input type="text" bind:value={employee.name} />
+      <label
+        for="employee_name"
+        class={employee.id ? 'active' : ''}>Name</label>
+      <input type="text" bind:value={employee.employee_name} />
     </div>
     <div class="input-field">
-      <label for="age">Age</label>
-      <input type="text" bind:value={employee.age} />
+      <label for="employee_age" class={employee.id ? 'active' : ''}>Age</label>
+      <input type="number" bind:value={employee.employee_age} />
     </div>
     <div class="input-field">
-      <label for="salary">Salary</label>
-      <input type="text" bind:value={employee.salary} />
+      <label
+        for="employee_salary"
+        class={employee.id ? 'active' : ''}>Salary</label>
+      <input type="number" bind:value={employee.employee_salary} />
     </div>
-    <button type="submit" class="waves-effect waves-light btn">Add </button>
+    <button
+      type="submit"
+      class="waves-effect waves-light btn">{employee.id ? 'Update' : 'Add'}</button>
   </form>
 {:else}
   <div class="progress">
--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -3,36 +3,46 @@
   import EmployeeForm from "../components/employeeform.svelte";
   const apiBaseUrl = "http://dummy.restapiexample.com/api/v1";
   let employees = [];
+  let editingEmployee = {
+    employee_name: "",
+    employee_age: "",
+    employee_salary: "",
+    id: null,
+  };
   onMount(async () => {
     const res = await fetch(apiBaseUrl + "/employees");
     let emp = await res.json();
     employees = emp.data;
   });
 
-  const editEmployee = (id) => {
-    console.log("edit emoployee", id);
+  const editEmployee = (employee) => {
+    window.scrollTo(0, 0);
+    console.log("edit emoployee", employee);
+    editingEmployee = employee;
   };
 
   const deleteEmployee = (id) => {
-    console.log("delete emoployee", id);
-    fetch(`${apiBaseUrl}/delete/${id}`, {
-      method: "DELETE",
-    })
-      .then((res) => {
-        return res.json();
+    if (confirm("Are you sure you want to delete ?")) {
+      fetch(`${apiBaseUrl}/delete/${id}`, {
+        method: "DELETE",
       })
-      .then((data) => {
-        //success
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          //success
+          alert(data.message);
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
   };
   const refreshList = (e) => {
     //data recieved from child component
     //update the rendered list here
-    console.log(e.detail);
+    editingEmployee = {};
+    alert(e.detail);
   };
 </script>
 
@@ -46,25 +56,28 @@
 </style>
 
 <div class="row">
-  <EmployeeForm on:postCreate={refreshList} />
+  <EmployeeForm
+    on:postCreate={refreshList}
+    on:postUpdate={refreshList}
+    {editingEmployee} />
 </div>
 <div class="row">
   {#if employees.length === 0}
     <h1>Loading...</h1>
   {:else}
-    {#each employees as { id, employee_salary, employee_age, employee_name }}
+    {#each employees as employee}
       <div class="col s6">
         <div class="card">
           <div class="card-content">
-            <p class="card-title">Name: {employee_name}</p>
-            <p>Salary: {employee_salary}</p>
-            <p>Age: {employee_age}</p>
+            <p class="card-title">Name: {employee.employee_name}</p>
+            <p>Salary: {employee.employee_salary}</p>
+            <p>Age: {employee.employee_age}</p>
             <div class="card-actions mgtp-10">
               <button
                 class="btn"
-                on:click={() => editEmployee(id)}>Edit</button>
+                on:click={() => editEmployee(employee)}>Edit</button>
               <button
-                on:click={() => deleteEmployee(id)}
+                on:click={() => deleteEmployee(employee.id)}
                 class="btn delete-btn">Delete</button>
             </div>
           </div>
